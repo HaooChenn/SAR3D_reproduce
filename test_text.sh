12598462f318c66befb45e6cb7af0eb56a921646
cyw-3d <yongweichencyw@gmail.com>
2025-05-16 19:48:59 +0800

    update README

--- test_text.sh
+++ test_text.sh
@@ -10,12 +10,12 @@ torchrun --nproc_per_node=1 --nnodes=1 \
   --text_json_path ./test_files/test_text.json
 
 # Flexicubes Version
-torchrun --nproc_per_node=1 --nnodes=1 \
-  --rdzv-endpoint=localhost:2980 --rdzv_backend=c10d test.py \
-  --depth=16 --fp16=2  \
-  --vqvae_pretrained_path ./checkpoint/vqvae-flexicubes-ckpt.pt \
-  --ar_ckpt_path ./checkpoint/text-condition-ckpt.pth \
-  --save_path ./eval \
-  --flexicubes True \
-  --text_conditioned True \
-  --text_json_path ./test_files/test_text.json
+# torchrun --nproc_per_node=1 --nnodes=1 \
+#   --rdzv-endpoint=localhost:2980 --rdzv_backend=c10d test.py \
+#   --depth=16 --fp16=2  \
+#   --vqvae_pretrained_path ./checkpoint/vqvae-flexicubes-ckpt.pt \
+#   --ar_ckpt_path ./checkpoint/text-condition-ckpt.pth \
+#   --save_path ./eval \
+#   --flexicubes True \
+#   --text_conditioned True \
+#   --text_json_path ./test_files/test_text.json
