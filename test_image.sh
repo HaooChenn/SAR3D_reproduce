f9bd630a0b97149beec3f14c53cba4199d72ee30
HaoChen <HaoChenn.Eric@gmail.com>
2025-07-30 20:01:42 +0800

    Update test_image.sh

--- test_image.sh
+++ test_image.sh
@@ -2,8 +2,8 @@
 torchrun --nproc_per_node=1 --nnodes=1 \
   --rdzv-endpoint=localhost:2980 --rdzv_backend=c10d test.py \
   --depth=24 --fp16=2  \
-  --vqvae_pretrained_path ./checkpoint/vqvae-ckpt.pt \
-  --ar_ckpt_path ./checkpoint/image-condition-ckpt.pth \
+  --vqvae_pretrained_path ./checkpoints/vqvae-ckpt.pt \
+  --ar_ckpt_path ./checkpoints/image-condition-ckpt.pth \
   --save_path ./eval \
   --flexicubes False \
   --test_image_path ./test_files/test_images
@@ -12,8 +12,8 @@ torchrun --nproc_per_node=1 --nnodes=1 \
 # torchrun --nproc_per_node=1 --nnodes=1 \
 #   --rdzv-endpoint=localhost:2980 --rdzv_backend=c10d test.py \
 #   --depth=24 --fp16=2  \
-#   --vqvae_pretrained_path ./checkpoint/vqvae-flexicubes-ckpt.pt \
-#   --ar_ckpt_path ./checkpoint/image-condition-ckpt.pth \
+#   --vqvae_pretrained_path ./checkpoints/vqvae-flexicubes-ckpt.pt \
+#   --ar_ckpt_path ./checkpoints/image-condition-ckpt.pth \
 #   --save_path ./eval \
 #   --flexicubes True \
 #   --test_image_path ./test_files/test_images
